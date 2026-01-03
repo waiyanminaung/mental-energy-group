7208132921c7dbd9dbd19f80ac08d3b062b548ac
wyma-dss <waiyanminaung@dss.com.mm>
2026-01-03 03:07:31 -1000

    fix(ContentDetailModal): adjust layout for better content display and scrolling

--- src/app/(main)/services/components/ContentDetailModal.tsx
+++ src/app/(main)/services/components/ContentDetailModal.tsx
@@ -16,7 +16,7 @@ interface ContentDetailModalProps extends ModalProps {
 const ContentDetailModal = ({ data, closeModal }: ContentDetailModalProps) => {
   return (
     <ModalWrapper className="md:max-w-[700px]">
-      <div className="bg-white p-8 rounded-lg">
+      <div className="bg-white p-8 rounded-lg h-[90vh] flex flex-col">
         <div className="flex items-start justify-between  mb-5">
           <div className="inline-flex items-center text-sm text-gray-900">
             <div className="w-14 h-14 rounded-full overflow-hidden mr-4 relative bg-[#0c2139] border-5 border-[#0c2139]">
@@ -50,19 +50,22 @@ const ContentDetailModal = ({ data, closeModal }: ContentDetailModalProps) => {
           </button>
         </div>
 
-        <h1 className="mb-3 text-2xl font-extrabold leading-tight text-gray-900">
-          {data.title}
-        </h1>
+        {/* Content */}
+        <div className="flex-1 overflow-auto">
+          <h1 className="mb-3 text-2xl font-extrabold leading-tight text-gray-900">
+            {data.title}
+          </h1>
 
-        <Image
-          src={data.featured_image}
-          alt={data.title}
-          width={700}
-          height={400}
-          className="rounded-lg border border-gray-100"
-        />
-        <div className="mt-5">
-          <p>{data.content}</p>
+          <Image
+            src={data.featured_image}
+            alt={data.title}
+            width={700}
+            height={400}
+            className="rounded-lg border border-gray-100"
+          />
+          <div className="mt-5">
+            <p>{data.content}</p>
+          </div>
         </div>
       </div>
     </ModalWrapper>
